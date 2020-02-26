4ea9adcc07999a00f485bd191112a110d0657564
Danny <dannypen@gmail.com>
2020-02-26 16:42:56 +0000

    navbar ready

--- application/routes.py
+++ application/routes.py
@@ -6,3 +6,7 @@ from application import app
 @app.route("/home")
 def home():
     return render_template("home.html", title="Home")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
