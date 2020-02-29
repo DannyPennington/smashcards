f8d8e7b2c9c09c6e471115caac56a87667af558e
Danny <dannypen@gmail.com>
2020-02-29 17:53:17 +0000

    Added route for characters page

--- application/routes.py
+++ application/routes.py
@@ -5,9 +5,13 @@ from application.models import Character
 @app.route("/")
 @app.route("/home")
 def home():
-    chars = Character.query.all()
-    return render_template("home.html", title="Home", chars=chars)
+    return render_template("home.html", title="Home")
 
 @app.route("/about")
 def about():
     return render_template("about.html", title="About")
+
+@app.route("/characters")
+def characters():
+    chars = Character.query.all()
+    return render_template("characters.html", title="Fighters", chars=chars)
